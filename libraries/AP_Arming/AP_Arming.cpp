606c991aff8fa05f2e6de302a03b5e48fa274d42
Keerthana-draganfly <keerthana.radhakrishnan@draganfly.com>
2026-04-07 09:33:37 -0600

    Fix: Avoid PreArm failure during log downloads
    Updated AP_Arming.cpp to ignore the log downloading state during pre-arm checks.

--- libraries/AP_Arming/AP_Arming.cpp
+++ libraries/AP_Arming/AP_Arming.cpp
@@ -402,10 +402,10 @@ bool AP_Arming::logging_checks(bool report)
             check_failed(ARMING_CHECK_LOGGING, report, "No SD card");
             return false;
         }
-        if (AP::logger().in_log_download()) {
+        /*if (AP::logger().in_log_download()) {
             check_failed(ARMING_CHECK_LOGGING, report, "Downloading logs");
             return false;
-        }
+        }*/
     }
     return true;
 }
